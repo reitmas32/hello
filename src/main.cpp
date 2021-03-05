6e08648bb83ae2b618d647425509d46bd15a33a2
reitmas32 <rafa.zamora.rals@gmail.com>
2021-03-04 23:14:02 -0600

    Use C++20

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
+#include <concepts>
+
+struct Component
+{
+    int eid = 0;
+};
+
+struct MyCmp : Component
+{
+    /* data */
+};
+
+template <class T>
+requires std::is_base_of<Component, T>::value 
+void print_cmp(Component *cmp)
+{
+    std::cout << cmp->eid << '\n';
+    return;
+}
 
 int main(int argc, char const *argv[])
 {
-    std::cout<<"Hola Gabriel"<<'\n';
+    MyCmp my_cmp;
+    print_cmp<MyCmp>(&my_cmp);
     return 0;
 }
